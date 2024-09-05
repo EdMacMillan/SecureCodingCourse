2d4ee2df3ba7fa3709458df853615fb4d3937970
EdMacMillan <EdAMacMillan@outlook.com>
2024-09-05 11:22:55 -0400

    Update sim2ex1.c

--- sim2ex1.c
+++ sim2ex1.c
@@ -11,11 +11,7 @@ int main(int argc, char** argv) {
         fprintf(stderr, "Please provide the address of a file as an input.\n");
         return -1;
     }
-    char *fileName = argv[1];
-   
-    char userFileQuoted[BUFSIZE] = {0};
-    sprintf(userFileQuoted, "\"%s\"", fileName);
-    char command[BUFSIZE] = {0};
-//sprintf(command, "wc -c < ", userFileQuoted);
- //   system(command);
+    char cmd[BUFSIZE] = "wc -c < ";
+    strcat(cmd, argv[1]);
+    system(cmd);
 }
